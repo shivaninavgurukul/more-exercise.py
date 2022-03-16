f77a45d65a35f54e757e808269d5a9d76af4cb24
Shivani Mehta <91535945+shivaninavgurukul@users.noreply.github.com>
2022-03-16 10:29:10 +0530

    Add files via upload

---
+++ strong password.py
@@ -0,0 +1,18 @@
+# p=input("enter the your password , i will cheak:-")
+# small=0
+# capital=0
+# special=0
+# number=0
+# for i in range(len(p)):
+#     if p[i]>"a" and p[i]<"z":
+#         small+=1
+#     elif p[i]>="0" and p[i]<="9":
+#         number+=1
+#     elif p[i]>"A" and p[i]<"Z":
+#         capital+=1
+#     elif p[i]=="$" or p[i]=="#" or p[i]=="@" or p[i]=="&":
+#         special+=1
+# if small>0 and number>0 and capital>0 and special>0 :
+#     print("your password is strong password")
+# else:
+#     print("your password is not strong password . Please create strong password")            
